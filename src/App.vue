cdef7673399bb52d28494adf8858705b55811811
Kristin Norwig <kristin.norwig@gmail.com>
2023-12-14 15:03:53 +0100

    feat: adds detail view for every user

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <UserList />
+</template>
+
+<script>
+import UserList from "@/components/UserList.vue";
+
+export default {
+  name: "App",
+  components: {
+    UserList,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/UserListDetail.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="user-list__details">
+    <h2 class="user-list__details-hl">Details</h2>
+    <dl v-if="selectedUser">
+      <dt>name:</dt>
+      <dd>{{ selectedUser.name }}</dd>
+      <!-- <dd>Leanne Graham</dd>-->
+
+      <dt>email:</dt>
+      <dd>{{ selectedUser.email || "-" }}</dd>
+      <!-- <dd>Sincere@april.biz</dd>-->
+
+      <dt>phone:</dt>
+      <dd>{{ selectedUser.phone || "-" }}</dd>
+      <!--<dd>1-770-736-8031 x56442</dd> -->
+
+      <dt>company:</dt>
+      <dd>{{ selectedUser.company.name || "-" }}</dd>
+      <!-- <dd>Romaguera-Crona</dd>-->
+    </dl>
+    <p class="user-list__details-hint" v-else>
+      Es wurde kein Nutzer gefunden / ausgewählt
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    selectedUser: Object,
+  },
+};
+</script>
+
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+}
+.user-list__details {
+  width: 60%;
+  padding: 1rem;
+  color: #192a56;
+}
+.user-list__details-hl {
+  color: #00a8ff;
+}
+.user-list__details dd {
+  margin-bottom: 1rem;
+}
+.user-list__details-hint {
+  border: 2px solid #e84118;
+  border-radius: 5px;
+  color: #e84118;
+  padding: 0.5rem;
+}
+</style>
